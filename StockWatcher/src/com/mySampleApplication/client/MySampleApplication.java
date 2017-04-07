d10f57a693570ddc16e94d358e5791cabe91fa93
Kevin Byrne <kbyrne@clearwateranalytics.com>
2017-04-07 16:00:11 -0600

    Add panels, handlers.

--- StockWatcher/src/com/mySampleApplication/client/MySampleApplication.java
+++ StockWatcher/src/com/mySampleApplication/client/MySampleApplication.java
@@ -1,54 +1,65 @@
 package com.mySampleApplication.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
  */
-public class MySampleApplication implements EntryPoint {
+public class MySampleApplication implements EntryPoint
+{
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
 
-    /**
-     * This is the entry point method.
-     */
-    public void onModuleLoad() {
-        final Button button = new Button("Click me");
-        final Label label = new Label();
+	/**
+	 * Entry point method.
+	 */
+	public void onModuleLoad()
+	{
+		// Create table for stock data.
+		stocksFlexTable.setText(0, 0, "Symbol");
+		stocksFlexTable.setText(0, 1, "Price");
+		stocksFlexTable.setText(0, 2, "Change");
+		stocksFlexTable.setText(0, 3, "Remove");
 
-        button.addClickHandler(event -> {
-            if (label.getText().equals("")) {
-                MySampleApplicationService.App.getInstance().getMessage("Hello, World!", new MyAsyncCallback(label));
-            } else {
-                label.setText("");
-            }
-        });
+		// Assemble Add Stock panel.
+		addPanel.add(newSymbolTextBox);
+		addPanel.add(addStockButton);
 
-        // Assume that the host HTML has elements defined whose
-        // IDs are "slot1", "slot2".  In a real app, you probably would not want
-        // to hard-code IDs.  Instead, you could, for example, search for all
-        // elements with a particular CSS class and replace them with widgets.
-        //
-        RootPanel.get("slot1").add(button);
-        RootPanel.get("slot2").add(label);
-    }
+		// Assemble Main panel.
+		mainPanel.add(stocksFlexTable);
+		mainPanel.add(addPanel);
+		mainPanel.add(lastUpdatedLabel);
 
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("slot1").add(mainPanel);
+		// Move cursor focus to the input box.
+		newSymbolTextBox.setFocus(true);
 
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
+		addStockButton.addClickHandler(event -> addStock());
 
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
+		newSymbolTextBox.addKeyDownHandler(event ->
+										   {
+											   if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
+											   {
+												   addStock();
+											   }
+										   });
+	}
 
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
-    }
+	private void addStock()
+	{
+
+	}
 }
